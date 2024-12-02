33b76c73092710c834a8748e0dbaf7b44b1e9330
adhikarimilan <38108519+adhikarimilan@users.noreply.github.com>
2024-12-03 08:41:25 +1100

    fixed when the array has a single element or repeated query elemt

--- 1_find_target_in_array.py
+++ 1_find_target_in_array.py
@@ -1,8 +1,8 @@
 def locate_card(cards,query):
-    lo,hi=0, len(cards)
+    lo,hi=0, len(cards)-1
 
 #if the array is empty return -1
-    if(hi==0):
+    if(hi<0 or lo>len(cards)-1):
         return -1
     
     while lo<=hi:
@@ -14,8 +14,13 @@ def locate_card(cards,query):
         print('lo: ',lo,  ' hi: ',hi, ' mid: ', mid)
 
         if(mid_num==query):
-            print("query found at: ",mid)
-            return mid
+            #checking if the query element is repeated and is on the left
+            #first occurence of the element is returned
+            if(mid-1>=0 and cards[mid-1]==query):
+                hi=mid-1
+            else:
+                print("query found at: ",mid)
+                return mid#found
         elif (mid_num>query):
             lo=mid+1
         elif (mid_num<query):
@@ -25,5 +30,9 @@ def locate_card(cards,query):
         
 test_cases=[{input:{'cards':[1,2,3,4,5,7,9],'query':7},'output':5},{input:{'cards':[], 'query':-1}, 'output':5}]
 
-
-locate_card([9,6,4,2,0,-1],-1)
+print("Test case 1")
+locate_card([2],2)
+print("Test Case 2")
+locate_card([5,3,2,1,1],1)
+print("Test case 3")
+locate_card([5,-1,-1,-1,-1],-1)
